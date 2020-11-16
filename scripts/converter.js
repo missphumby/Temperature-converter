19a89bf8d1ec04dfc23b0425d6f274978267062c
Funby <jamiuaminat@yahoo.com>
2020-11-16 15:24:18 +0100

    ppp

--- scripts/converter.js
+++ scripts/converter.js
@@ -1,15 +1,27 @@
-function computeKg(){
-    const poundss = document.querySelector('.pound-input').value;
-    const weightinKg = document.querySelector('#kilogram');
-    
 
-    const resultinKg = (poundss* 0.45359237).toFixed(2)
-    
+//  c/5 = (f-32)/9
+    // f = (9c+160)/5
 
-     weightinKg.innerHTML = `Your weight in Kilogram is: ${resultinKg} `
-
-}
+    function computeTemp() {
+        let celc = document.getElementById('celc-input').value;
+        const result = document.getElementById('result');
+        const fahrent = document.getElementById('fahrent');
+        const celcius = document.getElementById('celcius');
 
 
+        f = ((9 * celc) + 160) / 5;
+        c= ((celc-32)* 5/9).toFixed(2)
+
+        if(fahrent.checked){
+        result.innerHTML = `${celc}<sup>0</sup>C is ${f}<sup>0</sup>F`
+        celc = ""
+        }else if(celcius.checked){
+         result.innerHTML = `${celc}<sup>0</sup>F is ${c}<sup>0</sup>C`
+         celc = ""
+        }else if(!celcius.checked && !fahrent.checked){
+            alert('please select Fahrenheit or Celcius')
+        }
+
+      }
 
     
